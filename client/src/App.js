1bce5bf2e4acbb577ed2fe3351c613b2da7b7b3f
Dylan <dylancn@hawaii.edu>
2019-11-26 19:08:58 -1000

    material ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import { Button, Modal, Backdrop, Fade } from '@material-ui/core';
+import SignUp from './components/login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <form method="POST" action='/api/users/register'>
-          <input type="text" name="firstName"></input>
-          <input type="text" name="lastName"></input>
-          <input type="text" name="username"></input>
-          <input type="text" name="password"></input>
-          <input type="text" name="passwordConfirm"></input>
-          <input type="text" name="email"></input>
-
-          <input type="submit"></input>
-        </form>
-
-        <br />
-
-        <form method="POST" action='/api/auth'>
-          <input type="text" name="username"></input>
-          <input type="text" name="password"></input>
-          <input type="submit"></input>
-        </form>
-      </header>
-    </div>
+      <SignUp/>
+    </div >
   );
 }
 
