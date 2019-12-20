98632e9bc59847498be4e4ab50aef63142874207
dylan-cn <dylancn@hawaii.edu>
2019-12-20 02:42:16 -1000

    Update Checkout.js
    Adding spinning indicator on checkout submission

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Typography, Container, Button, Grid, TextField, FormControl, InputLabel, Select } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { Typography, Container, Button, Grid, TextField, FormControl, InputLabel, Select, CircularProgress } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
@@ -9,51 +9,18 @@ const useStyles = makeStyles(theme => ({
         flexDirection: 'column',
         alignItems: 'center',
     },
+    spinner: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        marginTop: -12,
+        marginLeft: -12,
+    },
+    wrapper: {
+        margin: theme.spacing(1),
+        position: 'relative',
+    },
 }));
-
-// Send order request to server
-function sendOrder(e) {
-    e.preventDefault();
-
-    const { id } = JSON.parse(window.localStorage.getItem('user'));
-    const order = JSON.parse(window.localStorage.getItem('cart')).cart;
-
-    const orderData = {
-        id,
-        order,
-        location: window.localStorage.getItem("location"),
-    }
-
-    // send to server
-    fetch('/api/checkout', {
-        method: 'POST',
-        headers: {
-            'x-auth-token': JSON.parse(window.localStorage.getItem('user')).token,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(orderData)
-    })
-        .then(res => res.json())
-        .then(json => {
-            // checkout was a success
-            if (json.success) {
-                // remove cart from localstorage
-                window.localStorage.removeItem('cart');
-
-                alert("You order has been sent");
-            } else {
-                alert("There was an error sending order");
-            }
-        })
-        .catch(err => {
-            alert("It seems there is a problem with the server, try again later");
-        })
-}
-
-// Function for location
-function handleLocation(e) {
-    window.localStorage.setItem("location", e.target.value)
-}
 
 // Locations
 const locations = {
@@ -64,10 +31,60 @@ const locations = {
 export default function Checkout() {
     const classes = useStyles();
 
+    const [loading, setLoading] = useState(false);
+
     // Set default location
     useEffect(() => {
         window.localStorage.setItem("location", Object.keys(locations)[0]);
     }, []);
+
+    // Send order request to server
+    function sendOrder(e) {
+        e.preventDefault();
+        setLoading(true);
+
+        const { id } = JSON.parse(window.localStorage.getItem('user'));
+        const order = JSON.parse(window.localStorage.getItem('cart')).cart;
+
+        const orderData = {
+            id,
+            order,
+            location: window.localStorage.getItem("location"),
+        }
+
+        // send to server
+        fetch('/api/checkout', {
+            method: 'POST',
+            headers: {
+                'x-auth-token': JSON.parse(window.localStorage.getItem('user')).token,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(orderData)
+        })
+            .then(res => res.json())
+            .then(json => {
+                // checkout was a success
+                if (json.success) {
+                    // remove cart from localstorage
+                    window.localStorage.removeItem('cart');
+
+                    alert("You order has been sent");
+                } else {
+                    alert("There was an error sending order");
+                }
+            })
+            .catch(err => {
+                alert("It seems there is a problem with the server, try again later");
+            })
+            .finally(() => {
+                setLoading(false);
+            })
+    }
+
+    // Function for location
+    function handleLocation(e) {
+        window.localStorage.setItem("location", e.target.value)
+    }
 
     const temp = JSON.parse(window.localStorage.getItem('cart'));
     const cart = temp ? temp.cart : null;
@@ -162,9 +179,12 @@ export default function Checkout() {
                     </Grid>
 
                     <br />
-                    <Button variant="contained" type="submit" style={{ float: 'right' }}>
+                    <Button variant="contained" type="submit" className={classes.wrapper} disabled={loading} style={{ float: 'right' }}>
                         Submit order
-                </Button>
+                        {loading &&
+                            <CircularProgress size={16} className={classes.spinner} />
+                        }
+                    </Button>
                 </form>
                 :
                 <>
